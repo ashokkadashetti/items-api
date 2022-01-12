bb6dbfeaa4a360a8788653bbbc36043ca7c31efa
ashokkadashetti <kadashettiashok@gmail.com>
2022-01-12 19:08:15 +0530

    Init

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,41 @@
+module Api
+    module V1
+        class ItemsController < ApplicationController
+            def index
+                items = Item.order('created_at DESC')
+                render json: { status: 'Success', message: 'Presented items', items: items }, status: :ok
+            end
+
+            def show
+                item = Item.find(params[:id])
+                render json: { status: 'Success', message: 'Item found', item: item }, status: 302
+            end
+
+            def create
+                item = Item.new(item_params)
+                item.save!
+                render json: { status: 'Success', message: 'Item created', item: item }, status: 201
+            end
+
+            def update
+                item = Item.find(params[:id])
+                item.update!(item_params)
+                render json: { status: 'Success', message: 'Item updated', item: item }, status: :ok
+            end
+
+            def destroy
+                item = Item.find(params[:id])
+                item.destroy!
+                render json: { status: 'Success', message: 'Item deleted' }, status: :ok
+            end
+
+
+            private
+
+            def item_params
+                params.require(:item).permit(:code, :name, :price)
+            end
+
+        end
+    end
+end
